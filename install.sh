3acb7ce800b01b06619dd567e53dcdf0b8757571
Krzysztof Kepinski <krzysztof.kepinski@oneadvanced.com>
2022-07-28 10:14:05 +0200

    feat: working on my first tensorflow - for iris dataset

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-brew install pyenv
-pyenv install 3.9.13
-pyenv global 3.9.13
-pip install virtualenv
-virtualenv env
-source env/bin/activate
+brew install pyenv 
+pyenv install 3.9.13 
+pyenv global 3.9.13 
+pip install virtualenv 
+virtualenv env 
+source env/bin/activate 
 ipython kernel install --name "env" --user
-pip install matplotlib seaborn sklearn plotly babyplots xgboost tensorflow-macos
+pip install tensorflow-macos tensorflow-metal jupyter pandas matplotlib seaborn sklearn plotly babyplots xgboost
